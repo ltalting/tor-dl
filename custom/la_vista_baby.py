4fe0e356b55e51674d33e3b9f1f59fe3b736aa4f
Landon Alting <landon.alting@gmail.com>
2026-03-15 02:02:57 -0400

    oops

--- custom/la_vista_baby.py
+++ custom/la_vista_baby.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 def check_code(exit_code: Optional[int]) -> bool:
-    return isinstance(exit_code, int) and exit >= 0
+    return isinstance(exit_code, int) and exit_code >= 0
 
 def exit_script_with_code(exit_code: int = 0):
     if check_code(exit_code):
